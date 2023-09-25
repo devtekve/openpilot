8701219eb0168cf05519db5fb76adfb4e1572134
Miguel Fernandez <miguel.fernandez@cimpress.com>
2023-09-25 16:00:11 +0200

    Refactor CI scripts and add GitHub workflow
    The build_dev.sh script was removed and replaced with build.sh, publish.sh, and build_and_publish.sh in the release/ci directory. These scripts separate the steps of building the project, publishing the release, and running both actions respectively. A gitignore file was also added to avoid sensitive files being pushed.
    
    The new GitHub workflow file, build_and_publish_private_dev-c3.yaml, will trigger the build and publish steps on push or manual trigger. These changes enhance automation and workflow management in the development process.

---
+++ release/ci/build.sh
@@ -0,0 +1,32 @@
+#!/usr/bin/env bash
+
+set -e
+
+DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null && pwd)"
+cd $DIR
+
+# Take parameters as arguments
+SOURCE_DIR=$1
+OUTPUT_DIR=$2
+VERSION=$3
+
+# Check parameters
+if [ -z "$SOURCE_DIR" ] || [ -z "$OUTPUT_DIR" ]; then
+    echo "Error: No source or output directory provided."
+    exit 1
+fi
+
+if [ -z "$VERSION" ]; then
+    echo "Error: No version provided."
+    exit 1
+fi
+
+# Let's bring the submodules!
+git submodule update --remote --init --recursive
+
+# Build
+#docker compose -f ${SOURCE_DIR}/docker-compose.yml build
+docker compose -f ${SOURCE_DIR}/docker-compose.yml up
+
+# "Tagging"
+echo "#define COMMA_VERSION \"$VERSION-dev\"" > ${OUTPUT_DIR}/common/version.h
